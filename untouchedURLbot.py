41bd79b6def90cf32524ccadcf8ce764145f1075
Kharms <Kharms@users.noreply.github.com>
2014-07-13 13:43:24 -0400

    Changed lists to sets, added 404 logic, moved ignore logic to function

--- untouchedURLbot.py
+++ untouchedURLbot.py
@@ -2,28 +2,32 @@ import praw
 import time
 import datetime
 import warnings
+import requests
 #setup
 
 usrAgnt = 'untouchedURL bot 7.12.2014 checks new posts for mobile links | GITHUB'
 usr = 'untouchedURL' 
 pw = '' #password
-maker = '' #account to pass feedback to
+maker = '' #reddit account to pass feedback to
 
 r = praw.Reddit(usrAgnt) 
 r.login(usr,pw)
 
 touchHint = {'.m.':'.','//m.':'//','/.compact':'/','//mobile.':'//','//touch.':'//'} #search terms and translations
-ignoreDomains = ['mlb.com','m.memegen.com','m.braves.mlb.com','m.imgur.com','m.espn.go.com', 'm.mlb.com', 'm.youtube.com', 'm.politico.com', 'm.wpbf.com'] #these take care of themselves
-ignoreSubreddits = []
-processedPosts = []
+
+ignoreDomains = {'mlb.com','m.memegen.com','m.braves.mlb.com','m.imgur.com','m.espn.go.com', 'm.mlb.com', 'm.youtube.com', 'm.politico.com', 'm.wpbf.com','m.huffpost.com','m.bleacherreport.com','m.btownthings.com','m.bbc.com'} #these take care of themselves/are broken
+processDomains = {'en.m.wikipedia.org'}
+ignoreSubreddits = set()
+processedPosts = set()
 
 feedbackURL = "[How am I doing?](http://www.reddit.com/message/compose/?to=untouchedURL&amp;subject=untouchedURL%20feedback)" 
 feedbackSubject = u'untouchedURL feedback' 
 sourcecodeURL = "[Sourcecode](https://github.com/Kharms/untouchedURL)"
 
 
-def post_comment(post, commentTxt):
-    global ignoreSubreddits
+
+#posts comment.
+def post_comment(post, commentTxt, ignoreSubreddits):
     try:
         a = post.add_comment(commentTxt)
         return True
@@ -34,6 +38,47 @@ def post_comment(post, commentTxt):
             ignoreSubreddits.append(post.subreddit)
         return False
 
+
+
+#checks to see if we can skip post
+def check_post(post, ignoreDomains, ignoreSubreddits, processedPosts):
+    if post.over_18:
+        return False
+    if post.subreddit in ignoreSubreddits:
+        return False
+    if post.domain in ignoreDomains:
+        return False
+    if post.id in processedPosts:
+        return False
+    else:
+        return True
+        
+#requests module found at: http://stackoverflow.com/questions/1140661/python-get-http-response-code-from-a-url        
+#Checks domain to see if it is in processDomains, if not, checks to see if fix returns 404. If it does, adds to ignoreDomains, else adds to processDomains.
+def check_domain(newlink,domain,ignoreDomains,processDomains):
+    if domain in processDomains:
+        return True
+    else:
+        try:
+            req = requests.head(newlink)
+            if req.status_code == 404:
+                ignoreDomains.append(domain)
+                return False
+            if req.status_code == 400:
+                ignoreDomains.append(domain)
+                return False
+            if (req.status_code > 299) and (req.status_code<400):
+                print "Check 3XX: "+newlink
+                return True
+            if req.status_code == 200:
+                processDomains.append(domain)
+                return True
+            else:
+                return False    
+        except ConnectionError:
+            print "failed to connect"
+            return False
+        
 
 running = True
 runCount = 0
@@ -47,26 +92,29 @@ while running:
 #newPosts = r.get_subreddit('todayilearned').get_new(limit=1000)
     for post in newPosts:
         url = post.url.lower()
-        if ((post.subreddit not in ignoreSubreddits) and (post.domain not in ignoreDomains)):
+        if check_post(post,ignoreDomains,ignoreSubreddits,processedPosts):
+            processedPosts.append(post.id)
             if any(hint in url for hint in touchHint):
-                if ((post.id not in processedPosts) and (post.over_18 == False)):
-                    print '--------'
-                    print datetime.datetime.utcnow()
-                    print post.permalink
-                    for hint, replacement in touchHint.items():
-                        if hint in url:
-                            newlink = post.url.replace(hint,replacement)
-                            print post_comment(post,("Here is a non-mobile link: " + newlink + "\n \n" + feedbackURL + " | "+sourcecodeURL))
-#r.send_message(maker,"test","Here is a non-mobile link: " + newlink + "\n \n" + feedbackURL)
-#post.add_comment("Here is a non-mobile link: " + newlink + "\n \n" + feedbackURL)
-                            processedPosts.append(post.id)
+                for hint, replacement in touchHint.items():
+                    if hint in url:
+                        newlink = post.url.replace(hint,replacement)
+                        if check_domain(newlink,post.domain,ignoreDomains,processDomains):
+                            print post_comment(post,("Here is a non-mobile link: " + newlink + "\n \n" + sourcecodeURL + " | "+feedbackURL),ignoreSubreddits)
+ 
     time.sleep(20)
     runCount += 1
     if runCount == 90: 
         runCount = 0
         processedPosts = []
         mail = r.get_unread()
+        print "processDomains: ", processDomains
+        print "ignoreDomains: ", ignoreDomains
+        print "ignoreSubreddits: ", ignoresubreddits
         for m in mail:
             if m.subreddit == None:
                 if m.subject == feedbackSubject:
-                    r.send_message(maker,m.subject,m.body)
+                    r.send_message(maker,m.subject,m.body)      
+                    
+                    
+                    #r.send_message(maker,"test","Here is a non-mobile link: " + newlink + "\n \n" + feedbackURL)
+                    #post.add_comment("Here is a non-mobile link: " + newlink + "\n \n" + feedbackURL)
