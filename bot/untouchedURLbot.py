1873d6f8b239daf116a5af3a9ffe495ce7def9a8
Kharms <Kharms@users.noreply.github.com>
2014-07-14 23:22:47 -0400

    added subreddits to ignore

--- bot/untouchedURLbot.py
+++ bot/untouchedURLbot.py
@@ -18,7 +18,7 @@ touchHint = {'.m.':'.','//m.':'//','/.compact':'/','//mobile.':'//','//touch.':'
 
 ignoreDomains = {'mlb.com','m.memegen.com','m.braves.mlb.com','m.imgur.com','m.espn.go.com', 'm.mlb.com', 'm.youtube.com', 'm.politico.com', 'm.wpbf.com','m.huffpost.com','m.bleacherreport.com','m.btownthings.com','m.bbc.com'} #these take care of themselves/are broken
 processDomains = {'en.m.wikipedia.org'} #domain whitelist
-ignoreSubreddits = set('latterdaysaints')
+ignoreSubreddits = set('latterdaysaints', 'politics','WTF')
 processedPosts = set()
 
 feedbackURL = "[How am I doing?](http://www.reddit.com/message/compose/?to=untouchedURL&amp;subject=untouchedURL%20feedback)" 
@@ -117,9 +117,9 @@ while running:
         runCount = 0
         processedPosts = set()
         mail = r.get_unread()
-        #print "processDomains: ", processDomains
-        #print "ignoreDomains: ", ignoreDomains
-        #print "ignoreSubreddits: ", ignoreSubreddits
+        print "processDomains: ", processDomains
+        print "ignoreDomains: ", ignoreDomains
+        print "ignoreSubreddits: ", ignoreSubreddits
         for m in mail:
             if m.subreddit == None:
                 if m.subject == feedbackSubject:
