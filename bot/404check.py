3a03024c60946b3b6bd4d578fec0415c6ae8683d
eeue56 <eeue56@users.noreply.github.com>
2015-07-23 15:17:30 +0100

    cleaning up the code

--- bot/404check.py
+++ bot/404check.py
@@ -1,24 +1,25 @@
-def check_domain(newlink,domain,ignoreDomains,processDomains):
+def check_domain(newlink, domain, ignoreDomains, processDomains):
     if domain in processDomains:
         return True
-    else:
-        try:
-            status = requests.head(newlink).status_code
-            if status == 404:
-                ignoreDomains.add(domain)
-                return False
-            if status == 400:
-                ignoreDomains.add(domain)
-                return False
-            if (status > 299) and (status<400):
-                print "Check%s: "%(status)+newlink
-                processDomains.add(domain)
-                return True
-            if status == 200:
-                processDomains.add(domain)
-                return True
-            else:
-                return False    
-        except requests.ConnectionError:
-            print "failed to connect"
-            return False
+
+    status = None
+
+    try:
+        status = requests.head(newlink).status_code
+    except requests.ConnectionError:
+        print "failed to connect"
+        return False
+
+    if status == 404 or status == 400:
+        ignoreDomains.add(domain)
+        return False
+
+    if status > 299 and status < 400 :
+        print "Check%s: " % (status) + newlink
+        processDomains.add(domain)
+        return True
+
+    if status == 200:
+        processDomains.add(domain)
+        return True
+    return False   
