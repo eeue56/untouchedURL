75e0a4f5e6c9023a45fe383c5e02b09f2efe2d50
eeue56 <eeue56@users.noreply.github.com>
2015-07-23 15:19:09 +0100

    cleaning up the code

--- bot/commentCleanup.py
+++ bot/commentCleanup.py
@@ -3,23 +3,23 @@ import os
 import time
 
 
-usrAgnt = 'untouchedURL old comment cleanup. heroku 7.14.2014'
-usr = 'untouchedURL' 
-pw = os.environ['botPW'] #password
+userAgent = 'untouchedURL old comment cleanup. heroku 7.14.2014'
+user = 'untouchedURL' 
+password = os.environ['botPW'] #password
 
-r = praw.Reddit(usrAgnt) 
-r.login(usr,pw)
+r = praw.Reddit(userAgent) 
+r.login(user, password)
 
 comments = r.user.get_comments()
 
 def should_delete(comment):
     if comment.link_author == u'[deleted]':
         return True
-    if comment.score<1:
-        if (time.time()-comment.created_utc)>3600:
+
+    if comment.score < 1:
+        if (time.time() - comment.created_utc) > 3600:
             return True
-    else:
-        return False
+    return False
 
 for comment in comments:
     if should_delete(comment):
